1ecdbbfccd29e7739c74356718b5b903127d3773
sina2104 <79207960+sina2104@users.noreply.github.com>
2023-12-31 21:59:46 +0300

    changed task5

--- task5/task.py
+++ task5/task.py
@@ -1,47 +1,67 @@
 import numpy as np
-import json
 
-def flat_list(line):
-    res = []
-    for row in line:
-        res.extend(row)
-    return res 
+def task1(line):
+    ind_line, flat_line = [], []
+    pos = 0
+    for el in line:
+        if isinstance(el, int):
+            el = [el]
+        for val in el:
+            ind_line.append(pos)
+            flat_line.append(val)
+        pos += 1       
+    res = [[0 for _ in range(len(flat_line))] for _ in range(len(flat_line))]
 
-def task1(line):    
-    line = json.loads(line)
-    used = []
-    res = np.zeros((len(flat_list(line)), len(flat_list(line))))    
-    for elx in line:
-        for x in elx:
-            for ely in line:
-                for y in ely:
-                    if y not in flat_list(used):    
-                        res[int(x) - 1][int(y) - 1] = 1
-        used.append(elx)
+    for i in range(len(ind_line)):
+        for j in range(len(ind_line)):
+            if ind_line[flat_line.index(j + 1)] >= ind_line[flat_line.index(i + 1)]:
+                res[i][j] = 1
+   
     return res
 
 def task2(line_1, line_2):
-    res_1 = task1(line_1)
-    res_2 = task1(line_2)
-    res = np.ones((len(res_1[0]), len(res_1[0])))
-    for i in range(len(res_1[0]) - 1):
-        if res_1[i + 1][i] != res_2[i + 1][i] and res_1[i][i + 1] != res_2[i][i + 1]:
-            res[i + 1][i] = 0
-            res[i][i + 1] = 0            
+    line_1 = task1(line_1)
+    line_2 = task1(line_2)
+
+    line_1_trans = np.transpose(line_1)
+    line_2_trans = np.transpose(line_2)
+    res_1 = np.zeros((len(line_1_trans), len(line_1_trans)))
+    res_2 = np.zeros((len(line_1_trans), len(line_1_trans)))    
+    res = np.zeros((len(line_1_trans), len(line_1_trans)))
+    res_0 = []
+    for i in range(len(line_1_trans)):
+        for j in range(len(line_1_trans)):
+            res_1[i][j] = line_1[i][j] * line_2[i][j]
+            res_2[i][j] = line_1_trans[i][j] * line_2_trans[i][j]
+            res[i][j] = res_1[i][j] or res_2[i][j]
+            if res[i][j] == 0 and [j + 1, i + 1] not in res_0:
+                res_0.append([i + 1, j + 1])
+    return res_0
+
+
+def task(line_1, line_2, res_0):
+    res = []
+
+    for i, ex1 in enumerate(line_1):
+        line_1_set = set(ex1) if isinstance(ex1, list) else set([ex1])
+        
+        for j, ex2 in enumerate(line_2):
+            line_2_set = set(ex2) if isinstance(ex2, list) else set([ex2])
+
+            for val in line_1_set:
+                for el in res_0:
+                    if val in el and el not in res:
+                        res.append(el)
+                        break
+
+            inter = line_1_set.intersection(line_2_set)
+            if inter and not any(val in el for el in res_0):
+                res.append(list(inter) if len(inter) > 1 else inter.pop())
+
     return res
 
-def task(line_1, line_2):
-    res = task2(line_1, line_2)
-    ind_list = []
-    res_list = []
-    i = 0
-    while i  < len(res[0]):
-        if res[i][i - 1] == 0:
-            ind_list.pop(-1)
-            ind_list.append([str(i), str(i + 1)])
-        else: 
-            ind_list.append(str(i + 1))
-        i += 1
-    return json.dumps(ind_list)
 if __name__ == "__main__":
-    print(task('[["1","2"],["3","4","5"],"6","7","9",["8","10"]]','["1",["2","3"],"4",["5","6","7"],"8","9","10"]'))
+    line_1 = [1,[2,3],4,[5,6,7],8,9,10]
+    line_2 = [[1,2],[3,4,5],6,7,9,[8,10]]
+
+    print(task(line_1, line_2, task2(line_1, line_2)))
